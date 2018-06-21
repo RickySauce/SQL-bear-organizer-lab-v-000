e0947edf2890cf9a8f2ef62f6a930896f2b731c2
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-21 21:57:35 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 4, "M", "Francis", "Pissed-Off", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 1738, "F", "Dog-Colored", "Pissed-Off", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 44, "M", "Red", "Pissed-Off", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 12, "F", "Ugly", "Pissed-Off", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 4, "M", "REd", "Pissed-Off", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 4, "M", "FRANCIS", "Pissed-Off", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 4, "M", "FRANCIS", "Pissed-Off", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 4, "M", "FRANCIS", "Pissed-Off", alse);
