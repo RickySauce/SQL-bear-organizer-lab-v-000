189e2f06299a1111fece01374826e3f8a25f261d
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-21 21:58:52 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Mr. Chocolate", 4, "M", "Francis", "Pissed-Off", true);
+  VALUES ("Mr. Chocolate", 4, "M", "Francis", "Pissed_Off", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Rowdy", 1738, "F", "Dog-Colored", "Pissed-Off", true);
+  VALUES ("Rowdy", 1738, "F", "Dog_Colored", "Pissed_Off", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Tabitha", 44, "M", "Red", "Pissed-Off", true);
+  VALUES ("Tabitha", 44, "M", "Red", "Pissed_Off", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Sergeant Brown", 12, "F", "Ugly", "Pissed-Off", true);
+  VALUES ("Sergeant Brown", 12, "F", "Ugly", "Pissed_Off", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Melissa", 4, "M", "REd", "Pissed-Off", true);
+  VALUES ("Melissa", 4, "M", "Red", "Pissed_Off", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Mr. Chocolate", 4, "M", "FRANCIS", "Pissed-Off", true);
+  VALUES ("Grinch", 4, "M", "No_Color", "Pissed_Off", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Mr. Chocolate", 4, "M", "FRANCIS", "Pissed-Off", true);
+  VALUES ("wendy", 4, "M", "Dark", "Pissed_Off", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Mr. Chocolate", 4, "M", "FRANCIS", "Pissed-Off", alse);
+  VALUES (null, 67, "M", "Red", "Pissed_Off", alse);
